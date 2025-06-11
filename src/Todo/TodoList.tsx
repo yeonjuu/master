d0c5577769a357b5e9a5588eda2c668a6589e124
yeonjuu <yeonju9896@gmail.com>
2025-06-12 03:03:14 +0900

    Add valiation on todo

--- src/Todo/TodoList.tsx
+++ src/Todo/TodoList.tsx
@@ -1,18 +1,69 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { styled } from "styled-components";
+
+const ErrorContainer = styled.span`
+  color: red;
+  font-size: 12px;
+  margin-top: 5px;
+`;
+
+interface ITodo {
+  text: string;
+  category: "toDo" | "doing" | "done";
+  date: Date;
+}
+
+interface IForm {
+  toDo: string;
+  extraError?: string;
+}
 
 function ToDoList() {
-  const { register, handleSubmit } = useForm();
+  const [todo, setTodo] = useState<string[]>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>();
 
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.toDo.length < 2) {
+      setError("extraError", {
+        type: "manual",
+        message: "To do must be at least 1 characters long",
+      });
+    }
+
+    setTodo((prev) => [data.toDo, ...prev]);
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("toDo", { required: true })} placeholder="Write a to do..." />
+        <input
+          {...register("toDo", {
+            required: true,
+            validate: {
+              notEmpty: (value) => value.trim() !== "",
+            }, // trim을 사용하여 공백을 제거한 후 체크
+          })}
+          placeholder="Write a to do..."
+        />
+        {errors.toDo && <ErrorContainer>This field is required</ErrorContainer>}
+        {errors.extraError && <ErrorContainer>{errors.extraError.message}</ErrorContainer>}
         <button>Add</button>
       </form>
+      <hr />
+      <h1>To Do List</h1>
+      <ul>
+        {todo.map((item, index) => (
+          <li key={index}>
+            <span>{item}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
