08f2590b1bc6854d29434277453ae53b87e9f233
yeonjuu <yeonju9896@gmail.com>
2025-06-13 15:52:44 +0900

    Fix strickMode error at react-helmet

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { useRecoilState } from "recoil";
 import { isDarkMode } from "./atom";
 import { darkTheme, lightTheme } from "./style/theme";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
-  const isDark = useRecoilState(isDarkMode);
-  console.log(isDark);
+  const [isDark] = useRecoilState(isDarkMode);
 
   return (
-    <>
+    <HelmetProvider>
       <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
         <GlobalStyle />
         <Router />
         <ReactQueryDevtools initialIsOpen={false} />
       </ThemeProvider>
-    </>
+    </HelmetProvider>
   );
 }
 
